2c1f02f94d5b07779df942c353144daf4154d9d8
= <=>
2023-03-24 12:25:16 +0500

    commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -16,7 +16,7 @@ const auth = async (req, res, next) => {
   // const token = authHeader.split(' ')[1];
 
   try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    const payload = jwt.verify(token, 'process.env.JWT_SECRET');
     // console.log(payload)
     // attach the user request object
     // req.user = payload
--- models/User.js
+++ models/User.js
@@ -48,8 +48,8 @@ UserSchema.pre('save', async function () {
 })
 
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_LIFETIME,
+  return jwt.sign({ userId: this._id }, 'process.env.JWT_SECRET', {
+    expiresIn: '1d',
   })
 }
 
